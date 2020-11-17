ba93e209441617a660307527f784f6d460eb9aae
Minseok Kim <minseokkim@kaist.ac.kr>
2020-11-17 23:28:32 +0900

    Update README.md

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ This is the first sentence.
  
  
  
+ 
--- r.sh
+++ r.sh
@@ -1,4 +1,4 @@
 sed -i -e '$a\ ' README.md
 git add *
 git commit -m "Update README.md"
-printf 'minseokkim@kaist.ac.kr/nKIki2891!!'|git push
+git push | printf 'minseokkim@kaist.ac.kr/nKIki2891!!'
