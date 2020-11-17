ef932e3af50f8f1af89271c9debf7d03a0cbd5ee
Minseok Kim <minseokkim@kaist.ac.kr>
2020-11-17 23:15:37 +0900

    Update README.md

--- r.sh
+++ r.sh
@@ -1,4 +1,4 @@
 sed -i -e '$a\ ' README.md
 git add *
 git commit -m "Update README.md"
-git push | grep minseokkim@kaist.ac.kr | grep KIki2891!!
+git push | echo minseokkim@kaist.ac.kr | echo KIki2891!!
